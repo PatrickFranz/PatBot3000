afb8856bb18cfd441b9e1786ae05ffc136318a83
Patrick Franz <clymbing@gmail.com>
2018-08-04 23:29:42 -0700

    Added some responses for name and location.

--- src/utils/handleIntents.js
+++ src/utils/handleIntents.js
@@ -5,8 +5,11 @@ function handleIntents(intents){
   const intentsList = Object.keys(intents.entities)
   switch (intentsList[0]){
     case 'greetings':
-      
       return ["Oh hai!", 'Welcome!', 'Hi there!'];
+    case 'name':
+      return ["I am PatBot3000, but I was programmed by Patrick Franz"];
+    case 'location':
+      return ["I live in the San Francisco Bay area", 'I live in Redwood City, CA, USA'];
     default:
       return ['Sorry I don\'t understand', 'Hmmm... not really getting you.', 'Can you be more specific?'];
   }
